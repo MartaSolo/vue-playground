35ca40c29b6904f2e5d4f72951458bf90e13bb87
Marta Solomacha <solomacha.marta@gmail.com>
2022-10-28 19:13:49 +0200

    Add TheSummary component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheProducts from "@/components/TheProducts.vue";
+import TheSummary from "@/components/TheSummary.vue";
 </script>
 
 <template>
   <TheProducts />
+  <TheSummary />
 </template>
 
 <style scoped></style>
