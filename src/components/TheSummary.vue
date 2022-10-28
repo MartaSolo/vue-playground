623993dacc8c978f95af18230439e328e41427a9
Marta Solomacha <solomacha.marta@gmail.com>
2022-10-28 20:35:10 +0200

    add list, delete table

--- src/components/TheSummary.vue
+++ src/components/TheSummary.vue
@@ -1,27 +1,15 @@
 <script lang="ts" setup></script>
 
 <template>
-  <table>
-    <tr>
-      <th>Company</th>
-      <th>Contact</th>
-      <th>Country</th>
-    </tr>
-    <tr>
-      <td>Alfreds Futterkiste</td>
-      <td>Maria Anders</td>
-      <td>Germany</td>
-    </tr>
-    <tr>
-      <td>Centro comercial Moctezuma</td>
-      <td>Francisco Chang</td>
-      <td>Mexico</td>
-    </tr>
-  </table>
+  <ul>
+    <li>Coffee</li>
+    <li>Tea</li>
+    <li>Milk</li>
+  </ul>
 </template>
 
 <style lang="css" scoped>
 table {
   background-color: palegreen;
-} 
+}
 </style>
