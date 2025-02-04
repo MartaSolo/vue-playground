f2b44ec02497ecde2ae59f6a8778f26165f3dc98
Marta Solomacha <marta.solomacha@monterail.com>
2025-02-04 21:58:02 +0100

    Add TheCard and adjust App

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import TheProducts from "@/components/TheProducts.vue";
 import TheSummary from "@/components/TheSummary.vue";
 import TheInput from "@/components/TheInput.vue";
 import TheRadio from "@/components/TheRadio.vue";
+import TheCard from "@/components/TheCard.vue";
+import card from "@/mocks/card";
 </script>
 
 <template>
@@ -10,6 +12,11 @@ import TheRadio from "@/components/TheRadio.vue";
   <TheSummary />
   <TheInput />
   <TheRadio />
+  <TheCard
+    :title="card.title"
+    :description="card.description"
+    :kicker="card.kicker"
+  />
 </template>
 
 <style scoped></style>
---
+++ src/mocks/card.js
@@ -0,0 +1,14 @@
+export default {
+  title: {
+    mobile: "Mobile title",
+    desktop: "Desktop title",
+  },
+  kicker: {
+    mobile: "Kicker title",
+    desktop: "Kicker title",
+  },
+  description: {
+    mobile: "description title",
+    desktop: "description title",
+  },
+};
