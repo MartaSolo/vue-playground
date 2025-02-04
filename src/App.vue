da555bda2d49d8e3b451cf07d8ec07e51b4ba383
Marta Solomacha <marta.solomacha@monterail.com>
2025-02-04 21:31:02 +0100

    Add new components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import TheProducts from "@/components/TheProducts.vue";
 import TheSummary from "@/components/TheSummary.vue";
+import TheInput from "@/components/TheInput.vue";
+import TheRadio from "@/components/TheRadio.vue";
 </script>
 
 <template>
   <TheProducts />
   <TheSummary />
+  <TheInput />
+  <TheRadio />
 </template>
 
 <style scoped></style>
---
+++ src/components/TheInput.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+  data() {
+    return {
+      step: 1,
+    };
+  },
+  methods: {
+    onKeyUpEnter(event) {
+      console.log("onKeyUpEnter fired");
+      console.log("event.target", event.target);
+      event.target.blur();
+      this.step++;
+    },
+    nextStep() {
+      console.log("next step fired");
+      this.step++;
+      console.log("this.step", this.step);
+    },
+    previousStep() {
+      console.log("previous step fired");
+      this.step--;
+      console.log("this.step", this.step);
+    },
+  },
+};
+</script>
+
+<template>
+  <div v-if="step === 1">
+    <h3>Step 1</h3>
+    <label for="name">Name</label>
+    <input type="text" id="name" @keyup.enter="onKeyUpEnter" />
+  </div>
+  <div v-if="step === 2">
+    <h3>Step 2</h3>
+    <label for="surname">Surname</label>
+    <input type="text" id="surname" @keyup.enter="onKeyUpEnter" />
+  </div>
+  <div v-if="step === 3">
+    <h3>Step 3</h3>
+    <label for="surname">Surname</label>
+    <input type="text" id="surname" @keyup.enter="onKeyUpEnter" />
+  </div>
+  <button @click="previousStep">Previous</button>
+  <button @click="nextStep">Next</button>
+</template>
+
+<style scoped>
+div {
+  margin-top: 2rem;
+}
+
+label {
+  padding: 2rem;
+}
+button {
+  display: inline-block;
+  margin: 2rem;
+}
+</style>
